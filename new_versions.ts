2f29445406ed7452608f7f43f6a42f5053022822
Alberto Mengali <disberd@gmail.com>
2024-03-09 19:16:10 +0100

    first test commit

---
+++ new_versions.ts
@@ -0,0 +1,36 @@
+import { Octokit } from "octokit";
+import semver from "semver";
+
+const octokit = new Octokit({
+    auth: process.env.TOKEN,
+});
+
+async function next_unreleased(current_version = "v0.0.0") {
+  const iterator = octokit.paginate.iterator(
+    "GET /repos/{owner}/{repo}/releases",
+    {
+      owner: "plotly",
+      repo: "plotly.js",
+      per_page: 100,
+    }
+  );
+  current_version = semver.coerce(current_version ?? "v0.0.0")
+  let breakLoop = false
+  let smallest_version: string | null = null
+
+  for await (const { data: releases } of iterator) {
+    if (breakLoop) break
+    for (const release of releases) {
+        let this_version = release.name
+        if (semver.gt(this_version, current_version)) {
+            smallest_version = this_version
+        } else {
+            break
+        }
+    }
+  }
+
+  return smallest_version
+}
+
+console.log(await next_unreleased())
