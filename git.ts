750ea2d892b0ba66ae9d74ca695d65a6fceae1ed
Alberto Mengali <disberd@gmail.com>
2024-03-10 22:18:01 +0100

    update inputs

--- git.ts
+++ git.ts
@@ -2,11 +2,11 @@ import { $ } from "bun"
 import fs from "fs"
 
 console.log(await $`git config user.name`.text())
-console.log(await $`git config user.name "CI"`.text())
-console.log(await $`git config user.email "CI@example.com"`.text())
+console.log(await $`git config user.name "Add Plotly Version"`.text())
+console.log(await $`git config user.email "add_plotly_version@email.com"`.text())
 console.log(await $`git config user.name`.text())
 console.log(await $`git config user.email`.text())
 fs.writeFileSync("VERSION", process.env.INPUT_VERSION)
 console.log(await $`git add VERSION`.text())
-console.log(await $`git commit -m "add version $(cat VERSION)`.text())
+console.log(await $`git commit -m "add plotly.js version $(cat VERSION)`.text())
 console.log(await $`git push`.text())
