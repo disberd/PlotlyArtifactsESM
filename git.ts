0919d9cf007068365b567a92b1158eced73c478d
Alberto Mengali <disberd@gmail.com>
2024-03-10 21:46:16 +0100

    workflow input

--- git.ts
+++ git.ts
@@ -1,7 +1,8 @@
 import { $ } from "bun"
 
-console.log(await $`git config user.name`)
-console.log(await $`git config user.name "CI"`)
-console.log(await $`git config user.email "CI@example.com"`)
-console.log(await $`git config user.name`)
-console.log(await $`git config user.email`)
+console.log(await $`git config user.name`.text())
+console.log(await $`git config user.name "CI"`.text())
+console.log(await $`git config user.email "CI@example.com"`.text())
+console.log(await $`git config user.name`.text())
+console.log(await $`git config user.email`.text())
+console.log(process.env.INPUT_VERSION)
